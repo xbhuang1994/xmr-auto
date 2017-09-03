03362b3f9b6230ff306be80dbb18744e85817d4c
xbhuang's pc <xbhuang1994@gmail.com>
2017-09-04 00:40:58 +0800

    1.修正链路问题

--- win/start.cmd
+++ win/start.cmd
@@ -1,3 +1,3 @@
 @echo off
-xmrig.exe -o nicehash.gpu360.com:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS1WEZGWKA3v566F6vb.xbhuang -p x -k -o xmr-eu1.nanopool.org:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS1WEZGWKA3v566F6vb.xbhuang_back -p x -k --max-cpu-usage=85 --donate-level=1
+xmrig.exe -o xmr-us-west1.nanopool.org:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS1WEZGWKA3v566F6vb.xbhuang -p x -k -o xmr-eu1.nanopool.org:14444 -u 4JUdGzvrMFDWrUUwY3toJATSeNwjn54LkCnKBPRzDuhzi5vSepHfUckJNxRL2gjkNrSqtCoRUrEDAgRwsQvVCjZbS1WEZGWKA3v566F6vb.xbhuang_back -p x -k --max-cpu-usage=15 --donate-level=1
 pause
